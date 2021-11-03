ee00ed180b005c3d01f350e40eeb6802e1b2d34f
kn1025yu <sn22kzh@gmail.com>
2021-11-03 16:15:49 +0000

    [add]応用課題2 デバッグ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,13 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
-    @book.user_id = current_user.id
+    @book.user = current_user
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -43,7 +44,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body )
   end
 
 end
